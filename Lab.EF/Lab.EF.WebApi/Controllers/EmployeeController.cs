d9c701baa8a6e03571d7e4bdab4e26c601c70a59
nicogarcette <nicolasgarcette@outlook.com>
2023-03-26 19:59:37 -0300

    Se realizo la web api rest sobre la entidad Empleado

---
+++ Lab.EF/Lab.EF.WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,98 @@
+﻿using Lab.EF.Entities;
+using Lab.EF.Logic;
+using Lab.EF.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using HttpDeleteAttribute = System.Web.Http.HttpDeleteAttribute;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+using HttpPutAttribute = System.Web.Http.HttpPutAttribute;
+
+namespace Lab.EF.WebApi.Controllers
+{
+    public class EmployeeController : ApiController {
+
+        private readonly EmployeeLogic employeeLogic = new EmployeeLogic();
+
+        //GET api/employee
+        public IHttpActionResult Get() {
+
+            List<EmployeeViewModel> data = employeeLogic.GetAll().Select(e => new EmployeeViewModel {
+                Id = e.EmployeeID,
+                FirstName = e.FirstName,
+                LastName = e.LastName
+            }).ToList();
+
+            if (data.Count == 0) 
+                return Content(HttpStatusCode.NotFound, "Sin registros");
+            
+            return Ok(data);
+        }
+
+        // DELETE api/employee/{id}
+        [HttpDelete]
+        public IHttpActionResult Delete(int id) {
+
+            try {
+                employeeLogic.Delete(id);
+                return Ok("Empleado eliminado");
+
+            } catch (Exception) {
+
+                return BadRequest("ERROR! no es posible eliminar el registro.");
+            }
+        }
+
+        // POST  api/employee
+        [HttpPost]
+        public IHttpActionResult Create(EmployeeViewModel e) {
+
+            if (!ModelState.IsValid) 
+                return BadRequest("Datos invalidos!");
+            
+            try {
+
+                Employees employees = new Employees {
+                    EmployeeID = e.Id,
+                    FirstName = e.FirstName,
+                    LastName = e.LastName
+                };
+
+                employeeLogic.Insert(employees);
+                return Ok("Empleado agregado");
+
+            } catch (Exception) {
+
+                return BadRequest("ERROR");
+            }
+        }
+
+        // PUT  api/employee
+        [HttpPut]
+        public IHttpActionResult Update(EmployeeViewModel e) {
+
+            if (!ModelState.IsValid) 
+                return BadRequest("Datos invalidos!");
+
+            try {
+                Employees employees = new Employees {
+                    EmployeeID = e.Id,
+                    FirstName = e.FirstName,
+                    LastName = e.LastName
+                };
+
+                employeeLogic.Update(employees);
+                return Ok("Empleado modificado.");
+
+            } catch (Exception) {
+
+               return BadRequest("ERROR");
+            }
+        }
+
+    }
+}
---
+++ Lab.EF/Lab.EF.WebApi/Models/EmployeeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab.EF.MVC.Models {
+    public class EmployeeViewModel {
+
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,15}$", ErrorMessage = "Solo letras, Maximo 15 carateres.")]
+        public string FirstName { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,15}$", ErrorMessage = "Solo letras, Maximo 15 carateres.")]
+        public string LastName { get; set; }
+
+    }
+}
